0881701ccfaa8e49dc1dac5bc53c23366b30bea8
Маратик555 <amr404@bk.ru>
2022-10-17 19:40:10 +0300

    env

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const instance = axios.create({
-	baseURL: 'http://localhost:4000'
+	baseURL: process.env.REACT_APP_API_URL
 })
 
 instance.interceptors.request.use((config) => {
